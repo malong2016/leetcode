d4e19dbc5facddf4157c9aa3a3c2b03948a91982
malong2016 <1520992967@qq.com>
2021-06-12 21:49:24 +0800

    121512

--- src/main/java/com/kwl/data01/Data01Application.java
+++ src/main/java/com/kwl/data01/Data01Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-//测试改变0022
+//测试改变002275855
 @SpringBootApplication
 public class Data01Application {
 
