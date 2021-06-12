adbc2a800052ec85b3b511e5eb03d6ee2a05ff84
malong2016 <1520992967@qq.com>
2021-06-12 21:43:04 +0800

    测试intel的改变02

--- src/main/java/com/kwl/data01/Data01Application.java
+++ src/main/java/com/kwl/data01/Data01Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-//测试改变
+//测试改变0022
 @SpringBootApplication
 public class Data01Application {
 
